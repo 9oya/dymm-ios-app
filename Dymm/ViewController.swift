83394be6df09f9cb1849c94806631e7a214fd54d
Roy Goya <roy1goya@gmail.com>
2019-08-06 00:48:20 +0900

    Initial Commit

---
+++ Dymm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dymm
+//
+//  Created by eunsang lee on 06/08/2019.
+//  Copyright © 2019 9oya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
